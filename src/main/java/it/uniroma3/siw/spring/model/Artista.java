58bce5ea6e98f9b406fab60f49a56250bb5c06f9
FTocci <72250345+FTocci@users.noreply.github.com>
2021-05-23 10:30:12 +0200

    Creating Artista and Collection

---
+++ src/main/java/it/uniroma3/siw/spring/model/Artista.java
@@ -0,0 +1,48 @@
+package it.uniroma3.siw.spring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQuery(name = "findAllArtists", query = "SELECT a FROM Artista a")
+public class Artista {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(nullable=false)
+	private String nome;
+
+	@Column(nullable=false)
+	private String cognome;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+}
