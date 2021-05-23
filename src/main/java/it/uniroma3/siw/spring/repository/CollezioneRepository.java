176da98abb630288cf4ee1d7e006deb908e78f92
FTocci <72250345+FTocci@users.noreply.github.com>
2021-05-23 10:58:53 +0200

    Updating Collections

--- src/main/java/it/uniroma3/siw/spring/repository/CollezioneRepository.java
+++ src/main/java/it/uniroma3/siw/spring/repository/CollezioneRepository.java
@@ -1,5 +1,17 @@
 package it.uniroma3.siw.spring.repository;
 
-public class CollezioneRepository {
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.spring.model.Collezione;
+
+public interface CollezioneRepository extends CrudRepository<Collezione,Long>{
+	
+	public List<Collezione> findByNome(String nome);
+
+	public List<Collezione> findByNomeAndDescrizione(String nome, String descrizione);
+
+	public List<Collezione> findByNomeOrDescrizione(String nome, String descrizione);
 
 }
