5a9cdecc23b41e00a6502ba6c42c511dcac6d330
FTocci <72250345+FTocci@users.noreply.github.com>
2021-05-28 17:00:33 +0200

    Update AuthConfiguration.java

--- src/main/java/it/uniroma3/siw/spring/authentication/AuthConfiguration.java
+++ src/main/java/it/uniroma3/siw/spring/authentication/AuthConfiguration.java
@@ -1,19 +1,20 @@
 package it.uniroma3.siw.spring.authentication;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import static it.uniroma3.siw.spring.model.Credentials.ADMIN_ROLE;
 
 import javax.sql.DataSource;
 
-import static it.uniroma3.siw.spring.model.Credentials.ADMIN_ROLE;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 //import static it.uniroma3.siw.spring.model.Credentials.DEFAULT_ROLE;
 
 /**
@@ -61,7 +62,9 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter {
                 // logout paragraph: qui definiamo il logout
                 .and().logout()
                 // il logout è attivato con una richiesta GET a "/logout"
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutUrl("/logout")
+
                 // in caso di successo, si viene reindirizzati alla /index page
                 .logoutSuccessUrl("/index")        
                 .invalidateHttpSession(true)
